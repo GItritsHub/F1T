310ff6f39abda507115881b394207657c2dec499
GItritsHub <GItritsHub@github.com>
2024-04-28 01:06:01 +0200

    Add scripts to run Calibration for the Camera Sensors on the module

---
+++ IMX219_StereoModule/CalibWithImages.py
@@ -0,0 +1,49 @@
+import numpy as np
+import cv2
+import glob, pickle
+
+chessboardSize = (9,6) #e.g. on an IPad measure https://www.longervision.github.io/2017/03/16/ComputerVision/OpenCV/opencv-internal-calibration-chessboard/ s.t. 20mm
+frameSize = (1280,720)
+sensor_id = 0 #CHANGE w.r.t. id
+
+# termination criteria
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+objp = np.zeros((chessboardSize[0] * chessboardSize[1], 3), np.float32)
+objp[:,:2] = np.mgrid[0:chessboardSize[0],0:chessboardSize[1]].T.reshape(-1,2)
+
+size_of_chessboard_squares_mm = 25 #If you use an IPhone or real paper, simply measure with a ruler a decent size and change accordingly
+objp = objp * size_of_chessboard_squares_mm
+
+objpoints = [] # 3d point in real world space
+imgpoints = [] # 2d points in image plane.
+
+images = glob.glob(f'./F1T/IMX219_StereoModule/images_sensorid{sensor_id}/*.png')
+
+for image in images:
+
+    img = cv2.imread(image)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Find the chess board corners
+    ret, corners = cv2.findChessboardCorners(gray, chessboardSize, None)
+
+    # If found, add object points, image points (after refining them)
+    if ret == True:
+
+        objpoints.append(objp)
+        corners2 = cv2.cornerSubPix(gray, corners, (11,11), (-1,-1), criteria)
+        imgpoints.append(corners)
+
+        # Draw and display the corners
+        cv2.drawChessboardCorners(img, chessboardSize, corners2, ret)
+        cv2.imshow('img', img)
+        cv2.waitKey(1000)
+
+
+cv2.destroyAllWindows()
+ret, cameraMatrix, dist, rot_vector, trans_vector = cv2.calibrateCamera(objpoints, imgpoints, frameSize, None, None)
+
+# Save the camera calibration result for later use (we won't worry about rvecs / tvecs)
+pickle.dump((cameraMatrix, dist), open( f"calibration{sensor_id}.pkl", "wb" )) #If this doesn't automatically create the calib pickle in the directory drag and drop it to IMX219_StereoModule
--- IMX219_StereoModule/SyncedCameraFeed.py
+++ IMX219_StereoModule/SyncedCameraFeed.py
@@ -3,17 +3,18 @@ import numpy as np
 import datetime
 import time
 from threading import Thread, Lock
+import pickle
 
-VIDEO_WIDTH = 1920
-VIDEO_HEIGHT = 1080
+VIDEO_WIDTH = 1280
+VIDEO_HEIGHT = 720
 
 def gstreamer_pipeline(
         sensor_id = 0, 
         capture_width=VIDEO_WIDTH,
         capture_heigt=VIDEO_HEIGHT,
-        display_width=int(VIDEO_WIDTH/4),
-        display_height=int(VIDEO_HEIGHT/4),
-        framerate=20,
+        display_width=int(VIDEO_WIDTH),
+        display_height=int(VIDEO_HEIGHT),
+        framerate=30,
         flip_method=2
     ):
     return (
@@ -35,13 +36,22 @@ def gstreamer_pipeline(
     )
 
 class VideoStream:
-    def __init__(self, sensor_id=0):
+    def __init__(self, sensor_id=0, calibration_pickle=None):
         self.sensor_id = sensor_id
         self.gstreamer_pipeline = gstreamer_pipeline(sensor_id=self.sensor_id)
         self.cap = cv2.VideoCapture(self.gstreamer_pipeline)
         self.ret, self.frame = self.cap.read()
         self.started = False
         self.read_lock = Lock()
+        self.cameraMatrix, self.dist, self.newCameraMatrix, self.roi = self.read_pickle(calibration_pickle)
+
+    def read_pickle(self, calibration_pickle):
+        with open(calibration_pickle, 'rb') as file:
+            cameraMatrix, dist = pickle.load(file)
+        
+        newCameraMatrix, roi = cv2.getOptimalNewCameraMatrix(cameraMatrix, dist, (VIDEO_WIDTH,VIDEO_HEIGHT), 1, (VIDEO_WIDTH,VIDEO_HEIGHT))
+        
+        return cameraMatrix, dist, newCameraMatrix, roi
 
     def start(self):
         if self.started:
@@ -71,23 +81,33 @@ class VideoStream:
     def release(self):
         self.started = False
         self.thread.join()
-        
+
     def __exit__(self, exc_type, exc_value, traceback):
         self.cap.release()
     
 
 def show_dual_camera():
-    cap0 = VideoStream(0).start()
-    cap1 = VideoStream(1).start()
+    cap0 = VideoStream(0, './F1T/IMX219_StereoModule/calibration0.pkl').start()
+    cap1 = VideoStream(1, './F1T/IMX219_StereoModule/calibration1.pkl').start()
 
     while cap0.isOpened() and cap1.isOpened():
         ret0, frame0 = cap0.read()
         ret1, frame1 = cap1.read()
     
         if ret0 and ret1:
-            synced_frames = np.hstack((frame0, frame1))
-            cv2.imshow('Synced Frames', synced_frames)
-                
+            mapx0, mapy0 = cv2.initUndistortRectifyMap(cap0.cameraMatrix, cap0.dist, None, cap0.newCameraMatrix, (VIDEO_WIDTH,VIDEO_HEIGHT), 5)
+            mapx1, mapy1 = cv2.initUndistortRectifyMap(cap1.cameraMatrix, cap1.dist, None, cap1.newCameraMatrix, (VIDEO_WIDTH,VIDEO_HEIGHT), 5)
+            undistorted_frame0 = cv2.remap(frame0, mapx0, mapy0, cv2.INTER_LINEAR)
+            undistorted_frame1 = cv2.remap(frame1, mapx1, mapy1, cv2.INTER_LINEAR)
+            x0, y0, w0, h0 = cap0.roi
+            
+            undistorted_frame0 = undistorted_frame0[y0:y0+h0, x0:x0+w0]
+            undistorted_frame1 = undistorted_frame1[y0:y0+h0, x0:x0+w0]
+
+            synced_frames = np.hstack((undistorted_frame0, undistorted_frame1))
+            cv2.imshow('undist1', synced_frames)
+            
+                            
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
         
